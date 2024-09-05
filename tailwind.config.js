02db3a55389fe8d9691fac8f2cdf3e060a2bd2a2
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-09-06 01:12:35 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "*.html",
+    ".src/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkPurple: "#00072d",
+        lightPurple: '#001241',
+        yell: '#ffe033'
+      },
+      fontFamily: {
+        sans: ['Moderustic', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
