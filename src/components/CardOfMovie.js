861fa355dd6a706c1f3477868405b2e608738d7d
Serhii-Bolhovskyi <azeror72@gmail.com>
2025-01-11 01:28:51 +0100

    Add Release Date to CardOfMovies

--- src/components/CardOfMovie.js
+++ src/components/CardOfMovie.js
@@ -3,7 +3,6 @@ export default function movieCard({ movie }) {
     <div class="w-52 flex flex-col justify-center space-y-3">
         <img class="aspect-square object-fill" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
         <div class="flex flex-col space-y-3">
-            <div class="flex flex-col space-y-1">
                 <div class="flex items-start space-x-1">
                     <h1 class="text-white text-base font-semibold line-clamp-2 w-36 h-12" >
                     ${movie.title}
@@ -13,7 +12,8 @@ export default function movieCard({ movie }) {
                         <p class="text-yell text-base text-center">${movie.vote_average.toFixed(1)}</p>
                     </div>
                 </div>
-                <p class="text-white text-xs opacity-60 tracking-normal h-8">${movie.genres.join(', ')}</p>
+                <p class="text-white text-xs opacity-60 tracking-normal h-8">${movie.genres.join(', ')}</p>           
+                <p class="text-sm text-white text-opacity-60">${movie.release_date.split('-')[0]}</p>
             </div> 
         </div>
     </div>
